e35cf03777c7dd4e23614ba850746db358709200
Egor Ulybin <egorulybin@gmail.com>
2025-08-11 15:36:11 +0000

    project first submission

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [activeColor, setActiveColor] = useState('');
+  const [toggleLights, setToggleLights] = useState(false);
+  const [showPurpleLight, setShowPurpleLight] = useState(false);
+  const colors = ['red', 'yellow', 'green', ...(showPurpleLight ? ['purple'] : [])];
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleClick = (e) => {
+    const selectedColor = e.target.className.split(' ')[0].trim();
+    setActiveColor((prevActiveColor) => (prevActiveColor === selectedColor ? '' : selectedColor));
+  };
+
+  useEffect(() => {
+    if (toggleLights) {
+      let currentIndex = 0;
+      const interval = setInterval(() => {
+        setActiveColor(colors[currentIndex]);
+        if (currentIndex >= colors.length - 1) {
+          currentIndex = 0;
+        } else {
+          currentIndex++;
+        }
+      }, 500);
+      return () => clearInterval(interval);
+    }
+  }, [toggleLights, showPurpleLight]);
+
+  // ------------ TOGGLE ALL LIGHTS  -------------
+  const toggleAllLights = () => {
+    setToggleLights((prevToggleLightsState) => !prevToggleLightsState);
+  };
+
+  // ------------ TOGGLE PURPLE LIGHT  -------------
+  const togglePurpleLight = () => {
+    setShowPurpleLight((prevPurpleLightState) => !prevPurpleLightState);
+    const extendBody = document.querySelector('.traffic-light-body');
+    if (!showPurpleLight) {
+      extendBody.style.height = '500px';
+    } else {
+      extendBody.style.height = '375px';
+    }
+  };
+
+  return (
+    <div className=' d-flex justify-content-center flex-column align-items-center'>
+      <div onClick={handleClick} className='traffic-light-body pt-2'>
+        {colors.map((color, index) => {
+          return <div key={index} className={`${color} ${activeColor === color ? 'selected' : ''}`}></div>;
+        })}
+      </div>
+      <div className='traffic-light-stick'></div>
+      <div className='buttons-div'>
+        <button onClick={toggleAllLights} className='mx-2 btn btn-primary btn-lg'>
+          Toggle Lights
+        </button>
+        <button onClick={togglePurpleLight} className='mx-2 btn btn-secondary btn-lg'>
+          Toggle Purple
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
